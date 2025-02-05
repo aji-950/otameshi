b205d08d44aac83cd948d3eb2f3b88885e7b1370
aji-950 <circus02@icloud.com>
2025-02-05 14:26:52 +0900

    Add files via upload

---
+++ ccdonuts/header.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="common/reset.css">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100..900&display=swap" rel="stylesheet">
+        <title>C.C.Donuts</title>
+        <link rel="stylesheet" href="styles/style.css">
+    </head>
+
+    <body>
+
+        <header>
+            <nav>
+                <div class="navbar">
+                <div class="container navContainer">
+                    <input class="checkbox" type="checkbox" name="" id="" />
+                    <div class="hamburgerLines">
+                        <span class="line line1"></span>
+                        <span class="line line2"></span>
+                        <span class="line line3"></span>
+                    </div>
+
+                    <div class="ctopLogo">
+                        <img src="images/ccLogo.png" alt="お店のロゴ">
+                    </div>
+
+                    <div class="menuItems">
+                        <li><img src="images/ccLogo.png" alt=""></a></li>
+                        <li><a href="index.php">TOP</a></li>
+                        <li><a href="list.php">商品一覧</a></li>
+                        <li><a href="#">よくある質問</a></li>
+                        <li><a href="#">問い合わせ</a></li>
+                        <li><a href="#">当サイトのポリシー</a></li>
+                    </div>
+                </div>
+                </div>
+            </nav>
+
+            <div class="loginCart">
+                <ul>
+                <li><a href="login.php"><img src="images/loginLogo.png" alt="ログイン"></a></li>
+                <li><a href="cart.php"><img src="images/cartLogo.png" alt="カート"></a></li>
+                </ul>
+            </div>
+
+            <div class="sarchWindow">
+                <form action="#" class="searchForm1">
+                    <label>
+                        <input type="text" placeholder="">
+                    </label>
+                    <button type="submit" aria-label="検索"></button>
+                </form>
+            </div>
+
+        </header>
